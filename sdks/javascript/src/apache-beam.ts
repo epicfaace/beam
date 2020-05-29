9f62e34fe77b859566dab33d79fc2c2dc1c4941a
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-05-28 22:58:25 -0400

    basic stuff with serializing with fn api

--- sdks/javascript/src/apache-beam.ts
+++ sdks/javascript/src/apache-beam.ts
@@ -1,8 +1,24 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
+import {
+  PTransform,
+  Components,
+  Pipeline as Pipeline_
+} from '../model/generated/beam_runner_api_pb'
 export class Pipeline {
-  constructor(props: any) {
+  constructor(_props?: any) {
     // TODO: add pipelineoptions
+  }
+
+  build() {
+    const pipeline = new Pipeline_()
+    const transform = new PTransform()
+    transform.setUniqueName('unique name 1')
+
+    const components = new Components()
+    components.getTransformsMap().set('t', transform)
+    pipeline.setComponents(components)
+    return pipeline.toObject()
   }
 }
--- sdks/javascript/test/apache-beam.test.ts
+++ sdks/javascript/test/apache-beam.test.ts
@@ -1,14 +1,9 @@
-import DummyClass from "../src/apache-beam"
+import { Pipeline } from '../src/apache-beam'
 
 /**
  * Dummy test
  */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
-  })
-
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
-  })
+describe('Pipeline test', () => {
+  const p = new Pipeline()
+  console.error(p.build())
 })
